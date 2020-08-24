91e851e0cd5a9c1d870d601983b4121bf04f533e
flfk55 <flfk55@naver.com>
2020-08-24 18:14:44 +0900

    DaehanBank 레이아웃 및 jQuery 구현

---
+++ script/script.js
@@ -0,0 +1,39 @@
+$("#mainMenu").mouseover(function(){
+    $(".subMenu").stop().slideDown();
+});
+$("#mainMenu").mouseout(function(){
+    $(".subMenu").stop().slideUp();
+});
+
+$("#main #tabBtn>button").click(function(){
+    //멀티탭 버튼 활성화 표시 시작
+    $("#main #tabBtn>button").removeClass("tabBtnOn");
+    $(this).addClass("tabBtnOn");
+    //멀티탭 버튼 활성화 표시 끝
+    //멀티탭 버튼 내용 매칭 시작
+    $("#main .bbsContents").css({"display" : "none"});
+    var dataIdLink = $(this).attr("data-IDLink");
+    $("#"+dataIdLink).css({"display" : "block"});
+    //멀티탭 버튼 내용 매칭 끝
+})
+
+//레이어 팝업 시작
+$("#main #bbsNotice tr:first-child>td").click(function(){
+    $("#layerPopupBg").css({"display" : "block"});
+});
+
+$("#closeBtn>button").click(function(){
+    $("#layerPopupBg").css({"display" : "none"});
+});
+
+
+
+/*setInterval(fnSlide, 2000);
+function fnSlide() {
+    $("#slideFrame").animate(
+                            {"margin-left" : "-1200px"},
+                             1000,
+                             function() {
+                                 $("slideFrame").css({"margin-left" : "0"});
+                            });
+};*/
